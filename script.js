8dabc0d211d2661691cd701df68d1ed2af59165d
Awoyemi-Patrick <awoyemipatrick@petalmail.com>
2024-11-20 12:07:37 +0100

    Nav-bar and hero section Mobile responsive

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-/** */
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const timeElement = document.getElementById('time');
     const dateElement = document.getElementById('date');
 
+
+    /**---- Clock ---- */
     function updateTime() {
         const now = new Date();
 
@@ -28,4 +27,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     setInterval(updateTime, 1000);
     updateTime();
+
+    /**  ----Hamburger ----  */
+    const navWrapper = document.querySelector(".nav_wrapper");
+    const openIcon = document.querySelector(".hamburger .open");
+    let closeIcon;
+
+    openIcon.addEventListener("click", () => {
+        navWrapper.style.display = "block";
+        openIcon.style.display = "none";
+        openIcon.insertAdjacentHTML("afterend", `
+            <li class="close">
+                <img src="./assets/close_icon.svg" alt="Close Icon">
+            </li>
+        `);
+
+        closeIcon = document.querySelector(".hamburger .close");
+        closeIcon.addEventListener("click", () => {
+            navWrapper.style.display = "none";
+            closeIcon.remove();
+            openIcon.style.display = "block";
+        });
+    });
 });
